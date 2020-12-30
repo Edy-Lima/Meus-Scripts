66fb0480d4bfd7dd6f46a5f50a8a20ac0c88888f
Edy-Lima <76793185+Edy-Lima@users.noreply.github.com>
2020-12-30 19:06:28 -0300

    Add files via upload

---
+++ limpeza-do-ubuntu.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#Limpeza do ubuntu
+read -s -n1 -p "O que deseja fazer?
+  (L)=limpar
+  (A)=Atualizar e Reparar
+  (S)=Sair
+Digite => " resposta
+
+case "${resposta}" in
+  l|L|"")
+    echo "Limpando o Sistema =)"
+    echo "Digite a senha de usuário para continuar"
+    sudo rm -rf /var/tmp/*
+    sudo apt clean -y
+    sudo apt autoremove && sudo apt autoclean -y
+    sudo rm -rf ${HOME}/.local/share/Trash/*    
+  ;;
+
+  a|A)
+    echo "Atualizando e reparando o Sistema."
+    echo "Digite a senha de usuário para continuar"
+    sudo apt update && sudo apt full-upgrade -y
+    sudo dpkg --configure -a
+  ;;
+  s|S)
+    echo "Sair"
+  ;;
+  *)
+    echo -e "Erro: Opção inválida\n\n"
+    ${0}
+  ;;
+esac
