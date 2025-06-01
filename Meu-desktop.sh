bffc79b4d758a3347fda8b1f1bb0b33740642ddd
Edy-Lima <edivaldolima603@gmail.com>
2025-05-31 23:52:09 -0300

    Projeto em construção

--- Meu-desktop.sh
+++ Meu-desktop.sh
@@ -20,12 +20,22 @@ read opçao
 case $opcao in
      1)
 echo "Removendo snap e instalando flatpak....
-     # Edte comando vai remover todo o suporte ao snap e instalar flatpak com o gnome-software
+     # Edte comando vai remover todo o suporte ao snap e instalar flatpak com o gnome-software.
      sleep $TIME
+         sudo apt remove --purge snapd -y
+         sudo apt autoremove -y
+         clear
+         sudo apt update && sudo apt full-upgrade -y
+         sudo apt install flatpak -y
+         sudo apt install gnome-software-plugin-flatpak -y
+         flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo -y
+     clear
+         ;;
      2)
-echo "
-     #
+echo "Fazendo limpeza e atualizabdo o Sistema....
+     # Este comando limpa e atualiza o Sistema.
      sleep $TIME
+     sudo apt update && sudo apt full-upgrade -y
      3)
 echo "
      #
