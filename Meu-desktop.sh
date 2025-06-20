b32c753dc316300cd2e75cc7b291b12a9d3497b5
Edy-Lima <edivaldolima603@gmail.com>
2025-06-20 01:50:18 -0300

    Em construção.

--- Meu-desktop.sh
+++ Meu-desktop.sh
@@ -3,92 +3,63 @@
 # OBS: Este script é destinado a usuários do Ubuntu que desejam configurar seu sistema operacional de maneira rápida e eficiente.
 #
 TIME=2
-clear
-while true;do
-echo " "
-echo "Configurando meu Ubuntu!"
-echo " "
-echo "Escolha uma opção abaixo para começar!
 
-      1 - Remover snap e instalar flatpak
-      2 - Limpeza geral e atualizações
-      3 - Configurar meu Ubuntu
-      0 - Sair"
-echo " "
-echo -n "Opção escolhida: "
-read opcao
-case $opcao in
-     1)
-echo "Removendo snap..."
-     # Este comando vai remover todo o suporte ao snap e instalar flatpak com o gnome-software.
-     sleep $TIME
-      sudo apt remove --purge snapd -y
-      sudo apt autoremove -y
-      sudo rm -rf ~/snap /snap /var/snap /var/lib/snapd /var/cache/snapd
-      clear
-echo "Instalando suporte a flatpak..."
-     # Este comando instala todas as dependências para o flatpak.
-      sudo apt update && sudo apt full-upgrade -y
-      sudo apt install flatpak -y
-      sudo apt install gnome-software-plugin-flatpak -y
-      flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-      clear
-         ;;
-     2)
-echo "Fazendo limpeza e atualizando o Sistema..."
-     # Este comando limpa e atualiza o Sistema.
-     sleep $TIME
-      sudo rm -rf /var/tmp/*
-      sudo rm -rf ~/.thumbnails/normal/*
-      sudo rm -rf ~/.cache/thumbnails/normal/*
-      sudo rm -rf ${HOME}/.local/share/Trash/*
-      sudo apt update && sudo apt full-upgrade -y
-      clear
-         ;;
-     3)
-echo "Configurando seu desktop..."
-     # Este comando vai configurar seu sistema operacional. 
-      sleep $TIME
-      sudo apt update && sudo apt full-upgrade -y
-      sudo apt remove --purge libreoffice* -y 
-      clear
-echo "Desativando e excluindo Swap..."
-     # Este comando vai desativar e excluir o Swap do Ubuntu.
-      sleep $TIME
-      sudo swapoff /swapfile
-      sudo rm -f /swapfile
-      sudo sed -i '/\/swapfile/d' /etc/fstab
-      clear
-echo "Instalando Codecs extras..."
-     # Este comando vai instalar os codecs extras do Ubuntu.
-      sleep $TIME
-      sudo apt update && sudo apt upgrade -y
-      sudo apt install ubuntu-restricted-extras -y 
-      clear
-echo "Ativando suporte a exFAT no Ubuntu..."
-     # Este comando vai ativar o suporte a exFAT no Ubuntu.
-      sleep $TIME
-      sudo apt install exfatprogs ffmpeg -y
-      clear
-echo "Instalando programas essenciais..."
-     # Este comando vai instalar programas essenciais no Ubuntu.
-      sleep $TIME
-      sudo apt install git synaptic gdebi gnome-shell-extension-manager ffmpeg testdisk glabels gnome-tweaks -y
-      gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize-or-previews'
-      clear
-echo "Tudo pronto, agora vamos reiniciar o computador!"
-     # Este comando vai reiniciar o computador.
-      sleep $TIME
-      sudo reboot
-         ;;
-     0)
-echo "Saindo..."
-     # Este comando encerra o programa!
-      sleep $TIME
-      exit 0
-         ;;
-     *)
-echo "Opção inválida, tente novamente!"
-         ;;
-esac
+while true; do
+    opcao=$(zenity --list \
+        --title="Configuração do Ubuntu" \
+        --text="Escolha uma opção abaixo para começar:" \
+        --column="Opção" --column="Descrição" \
+        1 "Remover snap e instalar flatpak" \
+        2 "Limpeza geral e atualizações" \
+        3 "Configurar meu Ubuntu" \
+        0 "Sair")
+
+    case $opcao in
+        1)
+            zenity --info --text="Removendo snap e instalando flatpak..."
+            sleep $TIME
+            sudo apt remove --purge snapd -y
+            sudo apt autoremove -y
+            sudo rm -rf ~/snap /snap /var/snap /var/lib/snapd /var/cache/snapd
+            sudo apt update && sudo apt full-upgrade -y
+            sudo apt install flatpak -y
+            sudo apt install gnome-software-plugin-flatpak -y
+            flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+            zenity --info --text="Snap removido e Flatpak instalado!"
+            ;;
+        2)
+            zenity --info --text="Fazendo limpeza e atualizando o sistema..."
+            sleep $TIME
+            sudo rm -rf /var/tmp/*
+            sudo rm -rf ~/.thumbnails/normal/*
+            sudo rm -rf ~/.cache/thumbnails/normal/*
+            sudo rm -rf ${HOME}/.local/share/Trash/*
+            sudo apt update && sudo apt full-upgrade -y
+            zenity --info --text="Limpeza e atualização concluídas!"
+            ;;
+        3)
+            zenity --info --text="Configurando seu desktop..."
+            sleep $TIME
+            sudo apt update && sudo apt full-upgrade -y
+            sudo apt remove --purge libreoffice* -y
+            sudo swapoff /swapfile
+            sudo rm -f /swapfile
+            sudo sed -i '/\/swapfile/d' /etc/fstab
+            sudo apt update && sudo apt upgrade -y
+            sudo apt install ubuntu-restricted-extras -y
+            sudo apt install exfatprogs ffmpeg -y
+            sudo apt install git synaptic gdebi gnome-shell-extension-manager ffmpeg testdisk glabels gnome-tweaks -y
+            gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize-or-previews'
+            zenity --info --text="Configuração concluída! O computador será reiniciado."
+            sleep $TIME
+            sudo reboot
+            ;;
+        0)
+            zenity --info --text="Saindo..."
+            exit 0
+            ;;
+        *)
+            zenity --error --text="Opção inválida ou cancelada!"
+            ;;
+    esac
 done
