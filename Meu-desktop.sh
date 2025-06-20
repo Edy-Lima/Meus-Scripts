b2b0a0b41c897e546ce6f3c4aec87b17966322d6
Edy-Lima <edivaldolima603@gmail.com>
2025-06-20 01:32:25 -0300

    Atualização com IA em 20 de Junho de 2025

--- Meu-desktop.sh
+++ Meu-desktop.sh
@@ -10,84 +10,85 @@ echo "Configurando meu Ubuntu!"
 echo " "
 echo "Escolha uma opção abaixo para começar!
 
-      1 - Remover snap e instal flatpak
+      1 - Remover snap e instalar flatpak
       2 - Limpeza geral e atualizações
       3 - Configurar meu Ubuntu
       0 - Sair"
 echo " "
-echo -n "Opçao escolhida: "
+echo -n "Opção escolhida: "
 read opcao
 case $opcao in
      1)
-echo Removendo snap....
-     # Edte comando vai remover todo o suporte ao snap e instalar flatpak com o gnome-software.
+echo "Removendo snap..."
+     # Este comando vai remover todo o suporte ao snap e instalar flatpak com o gnome-software.
      sleep $TIME
-      sudo apt remove --purge snapd* -y
+      sudo apt remove --purge snapd -y
       sudo apt autoremove -y
+      sudo rm -rf ~/snap /snap /var/snap /var/lib/snapd /var/cache/snapd
       clear
-echo Instalando suporte a flatpak....
-     # Este comando instal todas as depedências para o flatpak.
+echo "Instalando suporte a flatpak..."
+     # Este comando instala todas as dependências para o flatpak.
       sudo apt update && sudo apt full-upgrade -y
       sudo apt install flatpak -y
       sudo apt install gnome-software-plugin-flatpak -y
-      flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo -y
+      flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
       clear
          ;;
      2)
-echo Fazendo limpeza e atualizabdo o Sistema....
+echo "Fazendo limpeza e atualizando o Sistema..."
      # Este comando limpa e atualiza o Sistema.
      sleep $TIME
       sudo rm -rf /var/tmp/*
-      sudo rm -vfr ~/.thumbnails/normal/*
-      sudo rm -f ~/.cache/thumbnails/normal/*
-      sudo rm -rf ${HOME}/.local/share/Trash/* 
+      sudo rm -rf ~/.thumbnails/normal/*
+      sudo rm -rf ~/.cache/thumbnails/normal/*
+      sudo rm -rf ${HOME}/.local/share/Trash/*
       sudo apt update && sudo apt full-upgrade -y
       clear
          ;;
      3)
-echo Configurando seu desktop....
+echo "Configurando seu desktop..."
      # Este comando vai configurar seu sistema operacional. 
       sleep $TIME
       sudo apt update && sudo apt full-upgrade -y
       sudo apt remove --purge libreoffice* -y 
       clear
-echo Desativar e excluir Swap....
+echo "Desativando e excluindo Swap..."
      # Este comando vai desativar e excluir o Swap do Ubuntu.
       sleep $TIME
-      sudo systemctl stop swap.img.swap -y
-      sudo systemctl mask swap.img.swap -y
+      sudo swapoff /swapfile
+      sudo rm -f /swapfile
+      sudo sed -i '/\/swapfile/d' /etc/fstab
       clear
-echo Instalando Codecs extras....
+echo "Instalando Codecs extras..."
      # Este comando vai instalar os codecs extras do Ubuntu.
       sleep $TIME
       sudo apt update && sudo apt upgrade -y
       sudo apt install ubuntu-restricted-extras -y 
       clear
-echo Ativando suporte a exfat no ubuntu....
-     # Este comando vai ativar o suporte a exfat no Ubuntu.
+echo "Ativando suporte a exFAT no Ubuntu..."
+     # Este comando vai ativar o suporte a exFAT no Ubuntu.
       sleep $TIME
-      sudo apt install exfat-fuse exfat-utils -y
       sudo apt install exfatprogs ffmpeg -y
       clear
-echo Instalando programas essenciais....
+echo "Instalando programas essenciais..."
      # Este comando vai instalar programas essenciais no Ubuntu.
       sleep $TIME
       sudo apt install git synaptic gdebi gnome-shell-extension-manager ffmpeg testdisk glabels gnome-tweaks -y
       gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize-or-previews'
       clear
-echo Tudo pronto , agora vamos reinicar o computador!
+echo "Tudo pronto, agora vamos reiniciar o computador!"
      # Este comando vai reiniciar o computador.
       sleep $TIME
       sudo reboot
          ;;
      0)
-echo Saindo....
+echo "Saindo..."
      # Este comando encerra o programa!
       sleep $TIME
       exit 0
          ;;
      *)
-echo Opçao invalida, tente novamente!
+echo "Opção inválida, tente novamente!"
          ;;
 esac
 done
