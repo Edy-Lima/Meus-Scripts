87816dd19b667c9cbf2846621d125b5171c50473
Edy-Lima <edivaldolima603@gmail.com>
2025-07-18 08:17:06 -0300

    Atualizado em 18 de Julho de 2025

--- Meu-desktop.sh
+++ Meu-desktop.sh
@@ -19,14 +19,19 @@ while true; do
         1)
             echo "Removendo snap e instalando flatpak..."
             sleep $TIME
-            sudo apt remove --purge snapd -y
-            sudo apt autoremove -y
-            sudo rm -rf "${HOME}/snap" /snap /var/snap /var/lib/snapd /var/cache/snapd
-            sudo apt update && sudo apt full-upgrade -y
-            sudo apt install flatpak -y
-            sudo apt install gnome-software-plugin-flatpak -y
-            sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-            echo "Snap removido e Flatpak instalado!"
+            # Remover Snap e pacotes relacionados
+            sudo systemctl stop snapd
+            sudo apt purge snapd -y
+            sudo rm -rf ~/snap /snap /var/snap /var/lib/snapd /var/cache/snapd
+            # Instalar Flatpak e GNOME Software
+            sudo apt update
+            sudo apt install flatpak gnome-software-plugin-flatpak gnome-software -y
+            # Adicionar repositório Flathub
+            sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+            echo "Snap removido, Flatpak e GNOME Software instalados com suporte ao Flathub."
+            echo "Reinicie o computador para aplicar as mudanças."
+            sleep $TIME
+            sudo reboot
             ;;
         2)
             echo "Fazendo limpeza e atualizando o sistema..."
@@ -43,11 +48,6 @@ while true; do
             sleep $TIME
             sudo apt update && sudo apt full-upgrade -y
             sudo apt remove --purge libreoffice* -y
-            if [ -f /swapfile ]; then
-                sudo swapoff /swapfile
-                sudo rm -f /swapfile
-                sudo sed -i '/\/swapfile/d' /etc/fstab
-            fi
             sudo apt update && sudo apt upgrade -y
             sudo apt install ubuntu-restricted-extras -y
             sudo apt install exfatprogs ffmpeg -y
