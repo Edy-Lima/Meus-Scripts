1be3026b98c56ea129f4958d60f4f4afe7b3ae6f
Edy-Lima <edivaldolima603@gmail.com>
2025-07-27 20:15:49 -0300

    Projeto em construção 27 de Julho de 2025

---
+++ Meu-Projeto.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# filepath: /home/edy/Meus-Scripts/instalar_programas.sh
+
+# Lista de programas disponíveis
+PROGRAMAS=$(zenity --list --checklist \
+  --title="Instalar Programas" \
+  --text="Selecione os programas que deseja instalar:" \
+  --column="Selecionar" --column="Programa" --column="Descrição" \
+  FALSE "vim" "Editor de texto" \
+  FALSE "git" "Controle de versão" \
+  FALSE "curl" "Transferência de dados" \
+  FALSE "htop" "Monitor de sistema" \
+  FALSE "gimp" "Editor de imagens" \
+  FALSE "vlc" "Reprodutor de mídia" \
+  FALSE "neofetch" "Visualizador do sistema" \
+  --separator=" ")
+
+# Verifica se o usuário selecionou algum programa
+if [ -z "$PROGRAMAS" ]; then
+  zenity --info --text="Nenhum programa selecionado."
+  exit 0
+fi
+
+# Confirmação
+zenity --question --text="Deseja instalar: $PROGRAMAS ?"
+if [ $? -eq 0 ]; then
+  gnome-terminal -- bash -c "sudo apt update && sudo apt install -y $PROGRAMAS; echo 'Instalação concluída!'; read"
+else
+  zenity --info --text="Instalação cancelada."
+fi
