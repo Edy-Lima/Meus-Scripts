43ed2ccb82d0c6cd27d319ef7b28420efca067b3
Edy-Lima <edivaldolima603@gmail.com>
2025-07-30 08:47:54 -0300

    Atualizado em 30 de Julhor de 2025

--- Meu-Ubuntu-config-pessoal.sh
+++ Meu-Ubuntu-config-pessoal.sh
@@ -19,27 +19,41 @@ sudo rm -rf /var/cache/snapd -y
 sudo apt remove --purge libreoffice* -y
 sudo apt autoremove -y
 
-# Iniciando as instalações de programas e configurações pessoais
+# Instalação de programas e configurações pessoais
 echo "Instalando programas e configurando o sistema..."
-sudo apt update
+sudo apt update && sudo apt full-upgrade -y
+# Instalação de programas essenciais
+echo "Instalando programas essenciais..."
 sudo apt install ubuntu-restricted-extras -y
-sudo apt install git synaptic gdebi p7zip-full gnome-shell-extension-manager ffmpeg testdisk glabels gnome-tweaks -y
+sudo apt install git synaptic gdebi p7zip-full gnome-shell-extension-manager ffmpeg testdisk glabels gnome-tweaks steam gparted -y
+# Instalação do suporte ao Flatpak
+echo "Instalando suporte ao Flatpak..."
 sudo apt install flatpak -y
 sudo apt install gnome-software-plugin-flatpak -y
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+# Configurando janelas do GNOME
+echo "Configurando janelas do GNOME..."
 gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize-or-previews'
+# Configurando o github
+echo "Configurando o GitHub..."
 sudo apt update && sudo apt full-upgrade -y
 git config --global user.name "Edy-Lima"
 git config --global user.email edivaldolima603@gmail.com
 sudo apt update && sudo apt full-upgrade -y
+# Instalar o obs-studio 
+echo "Instalando OBS Studio..."
 sudo add-apt-repository ppa:obsproject/obs-studio -y
 sudo apt update
 sudo apt install obs-studio -y
+# Instalar Google Chrome
 echo "Instalando Google Chrome...."
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt-get --fix-broken install -y
 rm google-chrome-stable_current_amd64.deb
+# Atualizar o sistema
+echo "Atualizando o sistema..."
+sudo apt update && sudo apt full-upgrade -y
 
 echo "Configurações efetuadas com sucesso!"
 
