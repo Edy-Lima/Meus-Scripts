424872054c85f1a4a3cfae6fcd1710f0dd8c9f65
Edy-Lima <edivaldolima603@gmail.com>
2025-07-31 10:01:13 -0300

    Em construção

--- instalador.py
+++ instalador.py
@@ -9,4 +9,6 @@ def instalar_programas(programas):
 if __name__ == "__main__":
     lista = input("Digite os nomes dos programas separados por espaço: ")
     programas = lista.strip().split()
+    # Adiciona git e gdebi à lista, evitando duplicatas
+    programas += [p for p in ['git', 'gdebi'] if p not in programas]
     instalar_programas(programas)
