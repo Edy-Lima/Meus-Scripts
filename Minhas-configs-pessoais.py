8029db7a81fb39dfb458068a3ef0a6f0907d6bf1
Edy-Lima <edivaldolima603@gmail.com>
2025-08-12 18:58:10 -0300

    Construção atualizada em 12 de agosto de 2025 as 18:57 hs

--- Minhas-configs-pessoais.py
+++ Minhas-configs-pessoais.py
@@ -55,6 +55,16 @@ def main():
     run("sudo apt update")
     run("sudo apt install obs-studio -y")
 
+    # Instalar vs-code
+    print("Instalando Visual Studio Code...")
+    run("wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg")
+    run("sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/")
+    run("sudo sh -c 'echo \"deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main\" > /etc/apt/sources.list.d/vscode.list'")
+    run("sudo apt install apt-transport-https -y")
+    run("sudo apt update && sudo apt full-upgrade -y")
+    run("sudo apt install code -y")
+    run("rm -f packages.microsoft.gpg") # Remove o arquivo de chave baixado após a instalação do VS Code.
+
     # Instalar Google Chrome
     print("Instalando Google Chrome....")
     run("wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb")
