d1dc16296fc5fe97e918c1c5a414098f041f1351
Edy-Lima <edivaldolima603@gmail.com>
2025-08-19 20:51:33 -0300

    Atualizado em 19 de agosto de 2025 as 20:51 hs

--- Meu-Ubuntu-config-pessoal.sh
+++ Meu-Ubuntu-config-pessoal.sh
@@ -114,6 +114,7 @@ sleep 3
      flatpak install flathub org.inkscape.Inkscape -y
      flatpak install flathub org.onlyoffice.desktopeditors -y
      flatpak install flathub org.shotcut.Shotcut -y  
+     flatpak install flathub org.openshot.OpenShot -y
 clear
 echo "Atualização  geral do sistema..."
 sleep 3
