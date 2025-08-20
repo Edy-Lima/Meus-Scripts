31b1859dab8c3ab557fb3f2bd2496d914239af15
Edy-Lima <edivaldolima603@gmail.com>
2025-08-19 21:30:09 -0300

    Em edição

--- Meu-Ubuntu-config-pessoal.sh
+++ Meu-Ubuntu-config-pessoal.sh
@@ -14,7 +14,7 @@ clear
 # Obs: Este comando remove o snap e todos os pacotes relacionados, incluindo o LibreOffice, faça por sua conta e risco.
 echo "Certifique-se de que 
 leu e analizou todos os comandos inclusos neste script antes de executá-lo."
-sleep 3
+sleep 5
 clear
 echo "Preparando para iniciar as configurações......." 
 sleep 3
