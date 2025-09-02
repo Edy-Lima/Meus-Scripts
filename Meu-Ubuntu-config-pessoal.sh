fc16715912fd18bc9e5e4613a8dddeaeb9711cb3
Edy-Lima <edivaldolima603@gmail.com>
2025-09-02 00:56:35 -0300

    Atualizado em 2 de setembro de 2025

--- Meu-Ubuntu-config-pessoal.sh
+++ Meu-Ubuntu-config-pessoal.sh
@@ -65,6 +65,12 @@ echo "Instalando programas adicionais com base no Ubuntu.deb..."
 sleep 3
      sudo apt install git gufw synaptic gdebi p7zip-full gnome-shell-extension-manager ffmpeg testdisk glabels gnome-tweaks steam gparted neofetch -y
      sudo apt install ubntu-advantage-tools -y
+     sudo apt install mesa-utils -y
+     sudo apt update
+sudo apt install \
+  intel-opencl-icd \
+  intel-level-zero-gpu level-zero \
+  intel-media-va-driver-non-free libmfx1 -y
 clear
 # Instalação do suporte ao Flatpak
 echo "Instalando suporte ao Flatpak..."
