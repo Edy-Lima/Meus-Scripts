3d19386266187f8c471fb685e0bee145b3e425d0
Edy-Lima <edivaldolima603@gmail.com>
2025-10-12 09:49:57 -0300

    Atualizado em 12 de outubro de 2025

--- Meu-Fedora42-config-pessoal.sh
+++ Meu-Fedora42-config-pessoal.sh
@@ -9,29 +9,6 @@
 echo "Iniciando o script para Fedora 42..."
 sleep 5
 clear
-# Remove o snapd e todos os pacotes relacionados (se existir)
-echo "Removendo o suporte ao Snap (se existir)..."
-sleep 5
-sudo dnf remove --assumeyes snapd
-sudo rm -rf /snap /var/snap /var/lib/snapd /var/cache/snapd
-sudo dnf autoremove --assumeyes
-clear
-# Instalação de programas essenciais
-echo "Instalando programas essenciais..."
-sleep 5
-sudo dnf install --assumeyes @core @base gnome-tweaks gnome-extensions-app git gufw p7zip p7zip-plugins gparted neofetch mesa-dri-drivers intel-media-driver ffmpeg testdisk glabels
-clear
-# Instalação do Flatpak
-echo "Instalando suporte ao Flatpak..."
-sleep 5
-sudo dnf install --assumeyes flatpak
-sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-clear
-# Configurando janelas do GNOME
-echo "Configurando janelas do GNOME..."
-sleep 5
-gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize-or-previews'
-clear
 # Configurando o GitHub
 echo "Configurando o GitHub..."
 sleep 5
@@ -53,11 +30,6 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo
 sudo dnf check-update
 sudo dnf install --assumeyes code
-clear
-# Ativar suporte a exFAT
-echo "Ativando suporte a exFAT..."
-sleep 5
-sudo dnf install --assumeyes exfatprogs ffmpeg
 clear
 # Instala programas via Flatpak
 echo "Instalando apps via Flatpak..."
